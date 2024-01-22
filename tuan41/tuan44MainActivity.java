86e9fe2f97584cdf5eaf6bf0ab2668dd11430776
thaonhien12 <154716498+thaonhien12@users.noreply.github.com>
2024-01-22 09:54:20 +0700

    Update

---
+++ tuan41/tuan44MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.firstapp.tuan41;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.firstapp.R;
+
+public class tuan44MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tuan44_main);
+    }
+}
